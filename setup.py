2be26edecc1162f0cc44b9a4aebdb943437a2218
jbauthess <115120603+jbauthess@users.noreply.github.com>
2026-05-13 10:39:57 +0200

    WIP : organize files using ros2 architecture
    We need now to suppress all nodes definition at the root of the project

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+package_name = 'tmplt_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='timdev',
+    maintainer_email='jean-baptiste.authesserre.ext@safrangroup.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "basic = tmplt_py.basic_node:main",
+            "publisher = tmplt_py.publisher_node:main",
+            "subscriber = tmplt_py.subscriber_node:main",
+            "server = tmplt_py.server_node:main",
+            "client = tmplt_py.client_node:main"
+        ],
+    },
+)
